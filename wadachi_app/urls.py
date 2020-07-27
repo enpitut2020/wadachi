7f4910ce112eb7e7cafec47016b989cde51cb3ab
Negishi Kanta <negishi.kanta.sw@alumni.tsukuba.ac.jp>
2020-07-28 07:14:45 +0900

    deploy succeed

--- wadachi_app/urls.py
+++ wadachi_app/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path
 from . import views
 
+# ユーザ登録py
+from django.urls import path
+
+
+
 urlpatterns = [
     path('', views.bridge_list, name='bridge_list'),
     path('bridge/<int:pk>/brick/new/', views.brick_new, name='brick_new'),
     path('bridge/<int:pk>/', views.brick_list, name='brick_list'),
     path('bridge/new/', views.bridge_new, name='bridge_new'),
+    
 ]
 
